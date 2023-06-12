f392626639729a5ccc34b0f80d08a292380de71f
Anastasiia Belova <nastasia_belova@mail.ru>
2023-06-12 16:29:56 +0300

    added test on softAssertions page

---
+++ src/test/java/hw_selenide/GithubTests.java
@@ -0,0 +1,50 @@
+package hw_selenide;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class GithubTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        //    Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void checkingJUnit5Test() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("Soft");
+        $$(".js-wiki-sidebar-toggle-display li")
+                .findBy(Condition.text("SoftAssertions"))
+                .should(exist)
+                .$("a").click();
+        $(".markdown-body")
+                .$$("h4")
+                .findBy(Condition.text("Using JUnit5 extend test class:"))
+                .sibling(0)
+                .shouldHave(text("""
+                   @ExtendWith({SoftAssertsExtension.class})
+                                 class Tests {
+                                   @Test
+                                   void test() {
+                                     Configuration.assertionMode = SOFT;
+                                     open("page.html");
+
+                                     $("#first").should(visible).click();
+                                     $("#second").should(visible).click();
+                                   }
+                                 }
+                """));
+        // sleep(5000);
+
+    }
+}
